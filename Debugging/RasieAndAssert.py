c76ce2449ec8f9fa2eded5277a7fca05146ed500
John McCray <mccrayjrob@gmail.com>
2022-03-20 08:49:09 -0400

    Raise and Assert

--- Debugging/RasieAndAssert.py
+++ Debugging/RasieAndAssert.py
@@ -1,12 +1,16 @@
 #! /usr/bin/env python3
 
+import traceback
 
 #this is basically how to 'throw' an error is JS
 #raise Exception("Error Message here")
 
 def boxPrint(symbol, width, height):
+  #gotta fail fast and early!
   if len(symbol) != 1:
     raise Exception('"symbol" needs to be astring lenght of 1.')
+  if (width < 2) or (height < 2):
+    raise Exception('"height" needs to be astring lenght of 1.')
 
   print(symbol * width)
 
@@ -15,3 +19,27 @@ def boxPrint(symbol, width, height):
   print(symbol * width)
 
 boxPrint('x', 15, 5)
+
+#We could also write the traceback (callstack) to a file with traceback.format_exc())
+
+errorfile= open('errorFile.txt', 'a')
+errorfile.write(traceback.format_exc())
+errorfile.close() #this block of code in the catch of try catch would just print the traceback to a file and you could still have the rest of your program run
+
+#Assertions and assert statement - a sanity check to make sure your code isn;t doing something obviously worng
+#a conditional that wil throw an exception
+
+market_2nd = {'ns': 'green', 'ew': 'red' }
+def changeLight(intersection):
+  for key in intersection.keys():
+    if intersection[key] == 'green':
+      intersection[key] = 'yellow'
+    elif intersection[key] == 'yellow':
+      intersection[key] = 'red'
+    elif intersection[key] == 'red':
+      intersection[key] = 'green'
+
+  assert 'red' in stoplight.values(), 'neither light is' + str(intersection)
+  #basically we know before writing the func that atleast one should be red at all time so we can trow and exception if it's not
+
+changeLight(market_2nd)
