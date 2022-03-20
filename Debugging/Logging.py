3f6f774cbe1e29a8ea7b5dd441a0c41e0b79b9b5
John McCray <mccrayjrob@gmail.com>
2022-03-20 09:36:10 -0400

    Debugging Tools

---
+++ Debugging/Logging.py
@@ -0,0 +1,21 @@
+#! /usr/bin/env python3
+
+import logging
+#set of code for logging in python
+logging.basicConfig(level=logging.DEBUG, format= '%(asctime)s - %(levelname)s - %(message)s')
+#you can also log to a file by passing a filename param
+#logging.basicConfig(filename='myLogdile.txt', level=logging.DEBUG, format= '%(asctime)s - %(levelname)s - %(message)s')
+logging.disable(logging.CRITICAL)#Turns off logging for the propram at the critical level or lower (debug, info, warning, error, critical)
+
+logging.debug('' % ())
+
+def factorial(n):
+  logging.debug('Starting factorial %s' % (n))
+  total = 1
+  for i in range(n + 1):
+    total *= i
+    logging.debug('i issi %s, total is %s' % (i, total))
+  logging.debug('Return value is' % (total))
+  return total
+
+
